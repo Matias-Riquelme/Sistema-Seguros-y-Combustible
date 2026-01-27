d09a7139eab952e04b030b5997f34ed4f6eaf7f3
Camilo Calderon <cam.calderono@duocuc.cl>
2026-01-27 12:10:25 -0300

    Add files via upload
    Se implemento Framework de SpringBoot 3.5.10

---
+++ BackendTad/tadGestionUsuario/src/main/java/tadGestionUsuario/TadGestionUsuarioApplication.java
@@ -0,0 +1,13 @@
+package tadGestionUsuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadGestionUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TadGestionUsuarioApplication.class, args);
+	}
+
+}
---
+++ BackendTad/tadGestionUsuario/src/test/java/tadGestionUsuario/TadGestionUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package tadGestionUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadGestionUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
