44a40e10fa1c85872ec176caae31417e3778d7c6
Camilo Calderon <cam.calderono@duocuc.cl>
2026-01-29 11:58:08 -0300

    Se implemento arquitectura de los microservicios

---
+++ MicroservicioDocumentacion/src/test/java/tad/MicroservicioDocumentacion/MicroservicioDocumentacionApplicationTests.java
@@ -0,0 +1,13 @@
+package tad.MicroservicioDocumentacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioDocumentacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ MicroservicioIncidenteSiniestros/MicroservicioIncidenteSiniestros/src/test/java/tad/MicroservicioIncidenteSiniestros/MicroservicioIncidenteSiniestrosApplicationTests.java
@@ -0,0 +1,13 @@
+package tad.MicroservicioIncidenteSiniestros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioIncidenteSiniestrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
